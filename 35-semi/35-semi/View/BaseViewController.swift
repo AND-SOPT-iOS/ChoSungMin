bcd83ca7eb89e8fe7d4d11080c4f2ade18124f49
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-10-15 23:40:08 +0900

    feat : BaseViewController 생성후 적용

---
+++ 35-semi/35-semi/View/BaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseViewController.swift
+//  35-semi
+//
+//  Created by 조성민 on 10/15/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    func setStyle() {}
+    
+    func setUI() {}
+    
+    func setLayout() {}
+    
+}
