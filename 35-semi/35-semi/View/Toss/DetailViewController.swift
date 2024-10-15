104909db41c2c6507c55e052e4cd27c0bfd6ab53
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-10-15 23:21:03 +0900

    feat : DetailViewController 초기화

--- 35-semi/35-semi/View/Toss/DetailViewController.swift
+++ 35-semi/35-semi/View/Toss/DetailViewController.swift
@@ -9,33 +9,6 @@ import UIKit
 
 final class DetailViewController: UIViewController {
     
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 16, weight: .semibold)
-        
-        return label
-    }()
-    
-    private let contentLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 12, weight: .regular)
-        label.numberOfLines = 0
-        label.textAlignment = .left
-        
-        return label
-    }()
-    
-    private lazy var textField: UITextField = {
-        let textField = UITextField()
-        textField.layer.cornerRadius = 10
-        textField.layer.borderWidth = 1
-        textField.leftView = UIView(frame: CGRect(origin: .zero, size: .init(width: 10, height: 44)))
-        textField.leftViewMode = .always
-        textField.delegate = self
-        
-        return textField
-    }()
-    
     private lazy var backButton: UIButton = {
         let button = UIButton()
         button.setTitle("이전 화면으로", for: .normal)
@@ -46,23 +19,11 @@ final class DetailViewController: UIViewController {
         return button
     }()
     
-    private var receivedTitle: String?
-    private var receivedContent: String?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setStyle()
         setUI()
         setLayout()
-    }
-    
-    func dataBind(
-        title: String,
-        content: String
-    ) {
-        self.receivedTitle = title
-        self.receivedContent = content
-        updateUI()
     }
     
     @objc func backButtonTapped() {
@@ -82,57 +43,18 @@ private extension DetailViewController {
     }
     
     func setUI() {
-        [titleLabel, contentLabel, backButton, textField].forEach {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            self.view.addSubview($0)
+        [].forEach {
+//            $0.translatesAutoresizingMaskIntoConstraints = false
+//            self.view.addSubview($0)
         }
     }
     
     func setLayout() {
         NSLayoutConstraint.activate(
             [
-                titleLabel.topAnchor.constraint(
-                    equalTo: view.safeAreaLayoutGuide.topAnchor,
-                    constant: 20
-                ),
-                titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                contentLabel.topAnchor.constraint(
-                    equalTo: titleLabel.bottomAnchor,
-                    constant: 20
-                ),
-                contentLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                textField.topAnchor.constraint(
-                    equalTo: contentLabel.bottomAnchor,
-                    constant: 20
-                ),
-                textField.widthAnchor.constraint(equalToConstant: 300),
-                textField.heightAnchor.constraint(equalToConstant: 44),
-                backButton.topAnchor.constraint(
-                    equalTo: textField.bottomAnchor,
-                    constant: 20
-                ),
-                backButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                backButton.heightAnchor.constraint(equalToConstant: 44),
-                backButton.widthAnchor.constraint(equalToConstant: 300),
+                
             ]
         )
-    }
-    
-    func updateUI() {
-        self.titleLabel.text = receivedTitle
-        self.contentLabel.text = receivedContent
-    }
-    
-}
-
-extension DetailViewController: UITextFieldDelegate {
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        if (textField.text ?? "").count + string.count > 10 {
-            return false
-        }
-        return true
     }
     
 }
